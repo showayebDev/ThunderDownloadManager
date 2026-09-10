57bfcde276b5fe43c4823a6a6892dbdc2f491916
Showayeb Ahamed <showayeb.dev@gmail.com>
2026-09-10 23:02:55 +0600

    feat(updater): implement in-app auto-updater and release workflow

--- logger_dev.go
+++ logger_dev.go
@@ -9,25 +9,26 @@ import (
 	"os"
 )
 
-var wailsLogger *slog.Logger = slog.New(slog.NewTextHandler(io.Discard, nil))
+var wailsLogger *slog.Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 
 func initLogger() {
-	// In development builds, write to a debug file only if the --debug flag is explicitly passed.
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ldate | log.Ltime)
+
+	// In development builds, also write to a debug file if the --debug flag is explicitly passed.
 	for _, arg := range os.Args[1:] {
 		if arg == "--debug" || arg == "-d" {
 			logFile, err := os.OpenFile("ThunderDM_debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 			if err == nil {
-				log.SetOutput(logFile)
+				mw := io.MultiWriter(os.Stdout, logFile)
+				log.SetOutput(mw)
 				log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 				log.Println("--- DEV APP START ---")
-				wailsLogger = slog.New(slog.NewTextHandler(logFile, &slog.HandlerOptions{Level: slog.LevelDebug}))
+				wailsLogger = slog.New(slog.NewTextHandler(mw, &slog.HandlerOptions{Level: slog.LevelDebug}))
 			}
 			return
 		}
 	}
-	// Default: discard log output to keep console clean
-	log.SetOutput(io.Discard)
-	wailsLogger = slog.New(slog.NewTextHandler(io.Discard, nil))
 }
 
 func getWailsLogger() *slog.Logger {
